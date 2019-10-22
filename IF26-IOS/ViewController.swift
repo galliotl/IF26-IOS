d65b0e3e71a8f4b899cea7815877069c4f74caab
GALLIOT Lucas <galliotl@mac12-m102.utt.fr>
2019-10-22 10:34:19 +0200

    Initial Commit

---
+++ IF26-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IF26-IOS
+//
+//  Created by GALLIOT Lucas on 22/10/2019.
+//  Copyright © 2019 if26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IF26-IOSTests/IF26_IOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  IF26_IOSTests.swift
+//  IF26-IOSTests
+//
+//  Created by GALLIOT Lucas on 22/10/2019.
+//  Copyright © 2019 if26. All rights reserved.
+//
+
+import XCTest
+@testable import IF26_IOS
+
+class IF26_IOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
