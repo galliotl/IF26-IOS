994590aa375739e2687cc46a182eb5a352fc0c5f
GALLIOT Lucas <galliotl@mac09-m102.utt.fr>
2019-11-27 14:01:16 +0100

    commit before leaving

---
+++ IF26-IOS/ViewControllers/FeedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeedControllerViewController.swift
+//  IF26-IOS
+//
+//  Created by GALLIOT Lucas on 27/11/2019.
+//  Copyright © 2019 if26. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AppVariables.songs.count
+    }
+
+}
