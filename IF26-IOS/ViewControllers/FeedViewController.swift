4d18ac253977b6bcf963b22da02f5ac3872a3a16
GALLIOT Lucas <galliotl@mac09-m102.utt.fr>
2019-12-02 11:46:42 +0100

    Need to change audio session

--- IF26-IOS/ViewControllers/FeedViewController.swift
+++ IF26-IOS/ViewControllers/FeedViewController.swift
@@ -7,17 +7,75 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer = AVAudioPlayer()
+var feedsongs:[String] = []
+var thisSong = 0
+var audioStuffed = false
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        gettingSongNames()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return AppVariables.songs.count
+        return feedsongs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "feedcell")
+        cell.textLabel?.text = feedsongs[indexPath.row]
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do
+        {
+            let audioPath = Bundle.main.path(forResource: feedsongs[indexPath.row], ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(AVAudioSession.Category.playback)
+            audioPlayer.play()
+            thisSong = indexPath.row
+            audioStuffed = true
+        }
+        catch
+        {
+            print ("ERROR playing data")
+        }
+    }
+
+    func gettingSongNames() {
+        let folderURL = URL(fileURLWithPath:Bundle.main.resourcePath!)
+        
+        do
+        {
+            let songPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            for song in songPath
+            {
+                var mySong = song.absoluteString
+                if mySong.contains(".mp3")
+                {
+                    let findString = mySong.components(separatedBy: "/")
+                    mySong = findString[findString.count-1]
+                    mySong = mySong.replacingOccurrences(of: "%20", with: " ")
+                    mySong = mySong.replacingOccurrences(of: ".mp3", with: "")
+                    feedsongs.append(mySong)
+                }
+                
+            }
+            
+            myTableView.reloadData()
+        }
+        catch
+        {
+            print ("ERROR fetching data")
+        }
+    }    
 }
